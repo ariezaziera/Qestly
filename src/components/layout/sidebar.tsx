9fae4bbf148e023ab6da114574c559237794489e
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2026-06-17 00:22:52 +0800

    Update sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -38,7 +38,7 @@ export function Sidebar() {
           <div className="w-8 h-8 rounded-lg bg-primary flex items-center justify-center">
             <Radar size={16} color="white" />
           </div>
-          <span className="font-bold text-lg tracking-tight">JobRadar</span>
+          <span className="font-bold text-lg tracking-tight">Qestly</span>
         </div>
       </div>
 
