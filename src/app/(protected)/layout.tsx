294e2083ecb4d4332ef8941705202f4dd92c4576
Arieza Aziera <152407762+ariezaziera@users.noreply.github.com>
2026-06-19 09:51:53 +0800

    Update layout.tsx

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,6 +2,7 @@ import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
 import { Sidebar } from '@/components/layout/sidebar'
 import { MobileNav } from '@/components/layout/mobile-nav'
+import { MobileHeader } from '@/components/layout/mobile-header'
 import { InstallBanner } from '@/components/ui/install-banner'
 import { IdleTimeoutProvider } from '@/components/providers/idle-timeout-provider'
 
@@ -20,9 +21,15 @@ export default async function ProtectedLayout({
         <div className="hidden md:block">
           <Sidebar />
         </div>
-        <main className="flex-1 min-w-0 overflow-x-hidden md:ml-60 min-h-screen pb-20 md:pb-0">
-          {children}
-        </main>
+        <div className="flex-1 min-w-0 flex flex-col">
+          <MobileHeader
+            userEmail={user.email}
+            userAvatarUrl={user.user_metadata?.avatar_url}
+          />
+          <main className="flex-1 min-w-0 overflow-x-hidden md:ml-60 min-h-screen pb-20 md:pb-0">
+            {children}
+          </main>
+        </div>
         <MobileNav />
         <InstallBanner />
       </div>
